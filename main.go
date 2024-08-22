840ce4075041dcbd42cde1d0b8a6da6defec11c7
Artem <tema.butkov.03@bk.ru>
2024-08-22 15:25:01 +0300

    fix, add comments, rename variable

--- main.go
+++ main.go
@@ -14,96 +14,137 @@ import (
 )
 
 func main() {
+	fmt.Printf("\nЗапуск\n\n")
+
+	//метка старта программы
 	beginTime := time.Now()
 
-	params := readFlugs()
-	if params == nil {
-		return
+	//чтение флагов
+	pathLinks, pathDirectory, readFlugsErr := readFlugs()
+	if readFlugsErr != nil {
+		panic(readFlugsErr)
 	}
 
-	fmt.Println("Запуск")
-	err := readLinks(params[0], params[1])
-	if err != nil {
-		return
+	//парсинг ссылок, получение html и его сохранение
+	errReadLinksGetAndSaveHtml := readLinksGetAndSaveHtml(*pathLinks, *pathDirectory)
+	if errReadLinksGetAndSaveHtml != nil {
+		panic(errReadLinksGetAndSaveHtml)
 	}
+
+	//метка завершения программы
 	endTime := time.Now()
 
 	fmt.Printf("Время работы программы: %s\n", endTime.Sub(beginTime))
 
 }
 
-func saveHtml(path string, name string, html string) error {
+// сохранение html документа
+func saveHtml(savePath string, nameFile string, htmlDocument string) error {
 
-	pathFile := fmt.Sprintf("%s/%s.html", path, name)
+	//формирования пути сохранения
+	pathFile := fmt.Sprintf("%s/%s.html", savePath, nameFile)
 
-	file, err := os.Create(pathFile)
-	if err != nil {
+	//открытие файла
+	file, errOpenFile := os.Create(pathFile)
+	if errOpenFile != nil {
 
-		err = os.MkdirAll(path, 0777)
+		//создание пути сохранения, если он не найден
+		errMakePath := os.MkdirAll(savePath, 0777)
 
-		if err != nil {
-			fmt.Printf("Неудалось создать файл")
-			return errors.New("can't create file")
+		if errMakePath != nil {
+			errorStr := fmt.Sprintf("Неудалось создать файл %s\nОшибка:%s\n\n", pathFile, errMakePath)
+			return errors.New(errorStr)
 		}
-		file.Close()
-		file, err = os.Create(pathFile)
-		if err != nil {
-			fmt.Printf("Неудалось создать файл")
-			return errors.New("can't create file")
+
+		//повторная попытка окрытия
+		file, errOpenFile = os.Create(pathFile)
+		if errOpenFile != nil {
+			errorStr := fmt.Sprintf("Неудалось создать файл %s\nОшибка:%s\n\n", pathFile, errOpenFile)
+			return errors.New(errorStr)
 		}
 	}
 	defer file.Close()
-	file.WriteString(html)
+
+	file.WriteString(htmlDocument)
 	return nil
 }
 
-func readFlugs() []string {
-	pathLinks := flag.String("src", "input.txt", "path txt file")
-	pathDic := flag.String("dst", "targetDir", "path txt file")
+// чтение флагов
+func readFlugs() (*string, *string, error) {
+	pathLinks := flag.String("src", "", "path txt file")
+	pathDirectory := flag.String("dst", "", "path txt file")
 
 	flag.Parse()
 
-	result := []string{*pathLinks, *pathDic}
-	return result
+	//если флаг на файл с ссылками не установлен
+	if *pathLinks == "" {
+		return nil, nil, errors.New("не указан файл, содержащий ссылки")
+	}
+
+	//если флаг на директорию для сохранения не установлен
+	if *pathDirectory == "" {
+		fmt.Printf("Вы не указали директорию сохранения файлов.\nБудет выбрана директория по умолчанию (./targetDir)\n\n")
+		*pathDirectory = "targetDir"
+	}
+
+	return pathLinks, pathDirectory, nil
 }
 
-func readLinks(path string, savePath string) error {
-	file, err := os.Open(path)
+// чтение ссылок из файла, получение по ним html документа и запись его в файл
+func readLinksGetAndSaveHtml(pathInputFile string, savePath string) error {
+
+	//открытие файла с ссылками
+	file, err := os.Open(pathInputFile)
 	if err != nil {
-		fmt.Printf("Неудалось считать файл")
-		return nil
+		errorStr := fmt.Sprintf("Неудалось считать файл %s\n\n", pathInputFile)
+		return errors.New(errorStr)
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
 	var wg sync.WaitGroup
+
+	//чтение из файла
+	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		lineStr := scanner.Text()
-		lineStr = strings.TrimSpace(lineStr)
-		if lineStr != "" {
+
+		link := scanner.Text()
+		link = strings.TrimSpace(link)
+
+		//если не пустая строка в качестве ссылки,
+		//то выполнение запроса и сохранение html документа
+		if link != "" {
 			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				url := fmt.Sprintf("https://%s", lineStr)
-				response, err := http.Get(url)
-				if err != nil {
-					fmt.Printf("%s: ошибка запроса\n", lineStr)
-					return
-				}
-
-				defer response.Body.Close()
-
-				doc, _ := io.ReadAll(response.Body)
-				err = saveHtml(savePath, lineStr, string(doc))
-				if err != nil {
-					panic("can't save html file")
-				}
-				fmt.Printf("%s: успешно\n", lineStr)
-			}()
-
+			go getAndSaveHtmlFromLink(link, savePath, &wg)
 		}
 	}
 
 	wg.Wait()
 	return nil
+}
+
+// получение и сохранение html по ссылке
+func getAndSaveHtmlFromLink(link string, savePath string, waitGroup *sync.WaitGroup) {
+	defer waitGroup.Done()
+
+	//формирование url
+	link = strings.Trim(link, "htps:/")
+	url := fmt.Sprintf("https://%s", link)
+
+	//выполнение запроса и получение html
+	response, errGet := http.Get(url)
+	if errGet != nil {
+		fmt.Printf("%s: ошибка запроса:\n%s\n\n", link, errGet)
+		return
+	}
+
+	//чтение html документа из тела запроса и его сохранение
+	defer response.Body.Close()
+	responceBody, _ := io.ReadAll(response.Body)
+	errSaveHtml := saveHtml(savePath, link, string(responceBody))
+	if errSaveHtml != nil {
+		fmt.Printf("%s: неудалось создать файл:\n%s\n\n", link, errSaveHtml)
+		return
+	}
+
+	fmt.Printf("%s: успешно\n\n", link)
 }
